ee91a677313ef3233a1c2042baae5adad3f17f0c
halida <linjunhalida@gmail.com>
2016-02-13 10:48:26 +0800

    pass the test

--- csv/main.rkt
+++ csv/main.rkt
@@ -21,7 +21,7 @@
 ;; See the current version of the racket style guide here:
 ;; http://docs.racket-lang.org/style/index.html
 
-(provide read-csv write-csv csv-debug)
+(provide read-csv write-csv csv-debug row-reader)
 
 ;; Code here
 
@@ -37,67 +37,94 @@
           (printf "~n")
           )))
 
-(define (write-csv filename data
-                   #:field_delimiter [fd #\,]
-                   #:text_delimiter [td #\"])
-  (define (csv-writer f)
-    (define (row-writer row)
-      (define size (length row))
-      (for ([i size])
-           (define data (list-ref row i))
-           (define quote-string (string td))
-           (define quote-escape (format "~a~a" td td))
-           (define escaped (string-replace (format "~a" data)
-                                           quote-string quote-escape
-                                           #:all? #t))
-           (fprintf f "~a~a~a" td escaped td)
-           (unless (= i (- size 1))
-                   (fprintf f "~a" fd)))
-      (display "\n" f))
-    (map row-writer data))
-  (call-with-output-file filename #:exists 'truncate csv-writer))
+;; writer
+(define (csv-writer f data fd td)
+  (define (row-writer row)
+    (define size (length row))
+    (for ([i size])
+         (define data (list-ref row i))
+         (define quote-string (string td))
+         (define quote-escape (format "~a~a" td td))
+         (define escaped (string-replace (format "~a" data)
+                                         quote-string quote-escape
+                                         #:all? #t))
+         (fprintf f "~a~a~a" td escaped td)
+         (unless (= i (- size 1))
+                 (fprintf f "~a" fd)))
+    (display "\n" f))
+  (map row-writer data))
 
+(define (write-csv file data
+                   #:field-delimiter [fd #\,]
+                   #:text-delimiter [td #\"])
+  (define (writer f) (csv-writer f data fd td))
+  (cond
+   ([string? file]
+    (call-with-output-file file #:exists 'truncate writer))
+   ([output-port? file]
+    (writer file))
+   (else (error "file type unknown" file))
+   ))
 
-(define (read-csv filename
-                  #:field_delimiter [fd #\,]
-                  #:text_delimiter [td #\"])
-  (define (row-reader row)
-    (define row-chars (list->vector (string->list row)))
-    (define row-length (vector-length row-chars))
-    (define (state i s k)
-      (if [>= i row-length] ;; out of range
-          (if (eq? k '()) '()
-              (cons (list->string (reverse k)) '()))
-          (let* ([c (vector-ref row-chars i)]
-                 [is_delimiter (eq? c td)])
-            (log "i:~a c:~a s:~a k:~a" i c s k)
-            (case s
-              ([out]
-               (if is_delimiter
-                   (state (add1 i) 'in '())
-                   (state (add1 i) s k)))
-              ([in]
-               (if is_delimiter
-                   (state (add1 i) 'pout k)
-                   (state (add1 i) s (cons c k))))
-              ([pout]
-               (if is_delimiter
-                   ;; "" is escape for "
-                   (state (add1 i) 'in (cons td k))
-                   ;; out
-                   (cons (list->string (reverse k))
-                         (state i 'out '()))))
-              (else error "state error: ~a" s))
-            )))
-    (log "v:~a length:~a" row-chars row-length)
-    (state 0 'out '()))
-  (define (csv-reader f)
-    (define row (read-line f))
-    (if (eq? row eof) '()
-        (cons (row-reader row) (csv-reader f))))
-  (call-with-input-file filename csv-reader))
+;; reader
+(define (row-reader row fd td)
+  (define row-chars (list->vector (string->list row)))
+  (define row-length (vector-length row-chars))
+  (define (state i s k)
+    (if [>= i row-length] ;; out of range
+        (if (eq? k '()) '()
+            (cons (list->string (reverse k)) '()))
+        (let* ([c (vector-ref row-chars i)])
+          (log "i:~a c:~a s:~a k:~a" i c s k)
+          (case s
+            ([out] ;; out of each text
+             (cond
+               ([eq? c td] ;; in quoted text
+                (state (add1 i) 'inq '()))
+               ([member c (list fd #\space #\tab)] ;; seperates
+                (state (add1 i) s k))
+               (else ;; unquoted text
+                (state i 'inn k))))
+            ([inq] ;; in each text, quoted by text-delimiter
+             (cond
+               ([eq? c td] ;; pending out
+                (state (add1 i) 'pout k))
+               (else ;; append text
+                (state (add1 i) s (cons c k)))))
+            ([inn] ;; in each text, not quoted by text-delimiter
+             (cond
+               ([eq? c fd] ;; unquoted text out
+                (cons (list->string (reverse k))
+                      (state (add1 i) 'out '())))
+               (else ;; append text
+                (state (add1 i) s (cons c k)))))
+            ([pout] ;; pending out, encounter text-delimiter, check if it is escpaed delimiter
+             (cond
+               ([eq? c td] ;; "" is escape for "
+                (state (add1 i) 'inq (cons td k)))
+               (else ;; out
+                (cons (list->string (reverse k))
+                      (state i 'out '())))))
+            (else error "state error: ~a" s))
+          )))
+  (log "v:~a length:~a" row-chars row-length)
+  (state 0 'out '()))
 
+(define (csv-reader f fd td)
+  (define row (read-line f))
+  (if (eq? row eof) '()
+      (cons (row-reader row fd td) (csv-reader f fd td))))
 
-(module+ main
-  ;; Main entry point, executed when run with the `racket` executable or DrRacket.
-  )
+(define (read-csv file
+                  #:field-delimiter [fd #\,]
+                  #:text-delimiter [td #\"])
+  (define (reader f) (csv-reader f fd td))
+  (cond
+   ([string? file]
+    (call-with-input-file file reader))
+   ([port? file]
+    (reader file))
+   (else (error "file type unknown" file))))
+
+  
+
--- csv/test.rkt
+++ csv/test.rkt
@@ -1,14 +1,13 @@
 #lang racket
 
 (require "main.rkt")
-
 (require 2htdp/batch-io)
 
 (module+
  test
  (require rackunit)
 
- (csv-debug #t)
+ ;; (csv-debug #t)
 
  ;; Tests to be run with raco test
  (define data '(("name" "location")
@@ -17,14 +16,39 @@
                 ("Steven" "Austin")))
  (define csv-raw
    #<<HERESTRING
-"name","location"
-"James","Tokyo"
-"Bob","Shanghai"
-"Steven","Austin"
+'name','location'
+'James','Tokyo'
+'Bob','Shanghai'
+'Steven','Austin'
 HERESTRING
  )
  (define filename "xxx.csv")
- (write-csv filename data)
+ (write-csv filename data #:text-delimiter #\')
  (check-equal? (read-file filename) csv-raw)
- (check-equal? (read-csv filename) data)
+ (check-equal? (read-csv filename #:text-delimiter #\') data)
+
+ (define testcases-for-read-csv
+   '(
+     ;; normal quote
+     (("name, value, 1")
+      ("name" "value" "1"))
+     (("'name', 'value', '1'")
+      ("name" "value" "1"))
+     (("name, 'value', 1")
+      ("name" "value" "1"))
+     ;; ;; escape
+     (("name, 'val''ue', 1")
+      ("name" "val'ue" "1"))
+     (("name, 'va,lue', 1")
+      ("name" "va,lue" "1"))
+     (("name, val'ue, 1")
+      ("name" "val'ue" "1"))
+     ;; ;; error
+     (("name, 'value'', 1")
+      ("name" "value', 1"))
+     ))
+ (for ([item testcases-for-read-csv])
+      (let ([raw (caar item)]
+            [result (cadr item)])
+        (check-equal? (row-reader raw #\, #\') result)))
  )
